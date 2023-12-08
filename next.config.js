5b6dd3721b2bb72b18794a7d62d8133d4e324559
harshsahu12 <h693836@gmail.com>
2023-12-08 13:10:16 +0530

    new commit

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "digitalhippo-production.up.railway.app",
+        hostname: "hippoassets-production.up.railway.app/",
       },
     ],
   },
