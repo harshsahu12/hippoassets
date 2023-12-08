bd83923b905a4d969a101c2d515ff4b210240afe
harshsahu12 <h693836@gmail.com>
2023-12-08 14:00:57 +0530

    final commit

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "hippoassets-production.up.railway.app/",
+        hostname: "hippoassets-production.up.railway.app",
       },
     ],
   },
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { IncomingMessage } from "http";
 import { stripeWebhookHandler } from "./webhook";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 
@@ -43,6 +45,25 @@ const start = async () => {
       },
     },
   });
+
+  const cartRouter = express.Router() 
+
+  cartRouter.use(payload.authenticate)
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest
+
+    if(!request.user) return res.redirect("/sign-in?origin=cart")
+
+    const parseUrl = parse(req.url, true)
+
+    return nextApp.render(
+      req,
+      res,
+      "/cart",
+      parseUrl.query
+    )
+  })
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
